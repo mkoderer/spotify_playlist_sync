230f6c3e6320df99cb65cb995b86478247a8c957
Marc Koderer <marc@koderer.com>
2023-11-10 21:55:49 +0100

    Auto add

--- spotify_playlist_sync.py
+++ spotify_playlist_sync.py
@@ -1,3 +1,4 @@
+import argparse
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth, CacheFileHandler
 import logging
@@ -5,8 +6,12 @@ from pprint import pprint
 import yaml
 from mergedeep import merge, Strategy
 
+
+logging.basicConfig(level=logging.WARNING)
+log = logging.getLogger()
+
 def get_all(sp, function, *arg):
-    log.info("Call %s" % function)
+    log.info("Call get_all with function: %s" % function)
     func = getattr(sp, function)
     result = func(*arg)
     while result.get("next"):
@@ -15,15 +20,7 @@ def get_all(sp, function, *arg):
         merge(result, res, strategy=Strategy.ADDITIVE)
     return result
 
-
-logging.basicConfig(level=logging.WARNING)
-log = logging.getLogger()
-
-def main():
-    with open("config.yaml") as f:
-        config = yaml.full_load(f)
-
-    for account in config.get("accounts"):
+def auth(account, config):
         handler = CacheFileHandler(username=account["Username"])
         sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
@@ -33,18 +30,32 @@ def main():
                 cache_handler=handler,
                 open_browser=config.get("OpenBrowser"),
                 scope="playlist-read-collaborative playlist-read-private user-library-read user-library-modify"))
+        log.info("Authenicated with user %s" % account["Username"])
+        return sp
 
-        playlists = sp.current_user_playlists()
-        transfer_playlist = None
-        
-        for playlist in playlists['items']:
-            if config["SpotifyTransferPlaylist"] == playlist.get("name"):
-                transfer_playlist = sp.playlist(playlist_id=playlist["id"])
-                break
+def indentify_transfer_playlist(sp, config):
+    playlists = sp.current_user_playlists()
+    transfer_playlist = None
+    
+    for playlist in playlists['items']:
+        if config["SpotifyTransferPlaylist"] == playlist.get("name"):
+            transfer_playlist = sp.playlist(playlist_id=playlist["id"])
+            break
 
-        if transfer_playlist == None:
-            log.critical("No transfer playlist found (%s)" % config["SPOTIFY_TRANSFER_PLAYLIST"])
+    if transfer_playlist == None:
+        log.critical("No transfer playlist found (%s)" % config["SPOTIFY_TRANSFER_PLAYLIST"])
+        exit(1)
+    return transfer_playlist
 
+
+def main(args):
+    with open(args.config_file) as f:
+        config = yaml.full_load(f)
+    log.setLevel(config.get("LogLevel"))
+    for account in config.get("accounts"):
+        sp = auth(account, config)
+
+        transfer_playlist = indentify_transfer_playlist(sp, config)
         tracks_on_transfer = get_all(sp, "playlist_tracks", transfer_playlist.get("id"))
         tracks_id_trans = {}
         for track in tracks_on_transfer["items"]:
@@ -56,6 +67,26 @@ def main():
                 del tracks_id_trans[track.get("track").get("id")]
         print ("## Missing tracks:")
         pprint (tracks_id_trans)
+        if args.add and tracks_id_trans:
+            log.info("Adding missing tracks")
+            sp.current_user_saved_tracks_add(tracks_id_trans.keys())
+        
+    if args.empty_transfer:
+        sp = auth(config["accounts"][0], config)
+        transfer_playlist = indentify_transfer_playlist(sp, config)
+        # TODO remove all tracks
+
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description = 'Spotify playlist sync')
+    parser.add_argument('--config-file', "-c",
+                        help='Config file (default config.yaml)',
+                        default="config.yaml")
+    parser.add_argument('--add', action="store_true", default=False,
+                        help='Add missing tracks to saved tracks')
+    parser.add_argument('--empty-transfer', action="store_true", default=False,
+                        help='Remove all songs from transfer playlist')
+    args = parser.parse_args()
+
+    main(args)
