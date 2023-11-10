222ceba716a2dc7eb60d898f26723fb2ec901f3d
Marc Koderer <marc@koderer.com>
2023-11-10 21:26:28 +0100

    Use main function

--- spotify_playlist_sync.py
+++ spotify_playlist_sync.py
@@ -3,33 +3,59 @@ from spotipy.oauth2 import SpotifyOAuth, CacheFileHandler
 import logging
 from pprint import pprint
 import yaml
+from mergedeep import merge, Strategy
+
+def get_all(sp, function, *arg):
+    log.info("Call %s" % function)
+    func = getattr(sp, function)
+    result = func(*arg)
+    while result.get("next"):
+        log.debug("Next page for call %s" % function)
+        res = sp.next(result)
+        merge(result, res, strategy=Strategy.ADDITIVE)
+    return result
+
 
 logging.basicConfig(level=logging.WARNING)
-with open("config.yaml") as f:
-    config = yaml.full_load(f)
+log = logging.getLogger()
 
-for account in config.get("accounts"):
-    handler = CacheFileHandler(username=account["Username"])
-    sp = spotipy.Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=account.get("ClientId"),
-            client_secret=account["Secret"],
-            redirect_uri=account.get("RedirectUrl"),
-            cache_handler=handler,
-            scope="playlist-read-collaborative playlist-read-private user-library-read"))
+def main():
+    with open("config.yaml") as f:
+        config = yaml.full_load(f)
 
-    user = sp.current_user()
-    playlists = sp.current_user_playlists()
-    transfer_playlist = None
+    for account in config.get("accounts"):
+        handler = CacheFileHandler(username=account["Username"])
+        sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=account.get("ClientId"),
+                client_secret=account["Secret"],
+                redirect_uri=account.get("RedirectUrl"),
+                cache_handler=handler,
+                open_browser=config.get("OpenBrowser"),
+                scope="playlist-read-collaborative playlist-read-private user-library-read user-library-modify"))
 
-    for playlist in playlists['items']:
-        if config["SpotifyTransferPlaylist"] == playlist.get("name"):
-            transfer_playlist = sp.playlist(playlist_id=playlist["id"])
-            break
+        playlists = sp.current_user_playlists()
+        transfer_playlist = None
+        
+        for playlist in playlists['items']:
+            if config["SpotifyTransferPlaylist"] == playlist.get("name"):
+                transfer_playlist = sp.playlist(playlist_id=playlist["id"])
+                break
 
-    if transfer_playlist == None:
-        logging.critical("No transfer playlist found (%s)" % config["SPOTIFY_TRANSFER_PLAYLIST"])
+        if transfer_playlist == None:
+            log.critical("No transfer playlist found (%s)" % config["SPOTIFY_TRANSFER_PLAYLIST"])
 
-    tracks = sp.current_user_saved_tracks()
+        tracks_on_transfer = get_all(sp, "playlist_tracks", transfer_playlist.get("id"))
+        tracks_id_trans = {}
+        for track in tracks_on_transfer["items"]:
+            tracks_id_trans[track.get("track").get("id")] =  track.get("track").get("name")
 
-    pprint(tracks)
+        tracks = get_all(sp, "current_user_saved_tracks")
+        for track in tracks["items"]:
+            if track.get("track").get("id") in tracks_id_trans:
+                del tracks_id_trans[track.get("track").get("id")]
+        print ("## Missing tracks:")
+        pprint (tracks_id_trans)
+
+if __name__ == "__main__":
+    main()
